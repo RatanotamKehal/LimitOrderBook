2b8c1900627b643a18421dc3f63b2cd4614c1977
Ratanotam Kehal <47280941+RatanotamKehal@users.noreply.github.com>
2026-09-04 16:03:09 -0700

    Updated bench_lob.cpp
    Only need to create timing loop now!
    
    ALMOST DONE!

--- benchmarks/bench_lob.cpp
+++ benchmarks/bench_lob.cpp
@@ -20,20 +20,45 @@ static void BM_OrderBook(benchmark::State& state) {
 	std::uniform_int_distribution<int> price_distrib(495, 505);
 	std::uniform_int_distribution<int> qty_distrib(1, 100);
 	std::uniform_int_distribution<int> side_distrib(0, 1);
-
+	OrderID current_id = 0;
 
 	for (int i = 0; i < COMMAND_COUNT; i++) {
 		int roll = probability_distrib(gen);
 		Price price = price_distrib(gen);
 		Quantity qty = qty_distrib(gen);
-		bool side = side_distrib(gen);
+		Side side = static_cast<Side>(side_distrib(gen));
 
 
-
+		Order order;
 		if (roll <= 70) {
-			commands.push_back(Command{ false, Order{ } });
+			order = {
+				current_id++,
+				price,
+				qty,
+				0,
+				OrderType::Limit,
+				side,
+			};
+			active_limits_ids.push_back(order.order_id);
+			commands.push_back(Command{false, order});
 		}
-		commands.push_back(Command{});
+		else if (roll <= 90) {
+			std::uniform_int_distribution<int> list_distrib(0, active_limits_ids.size());
+			auto random_index = list_distrib(gen);
+			commands.push_back(Command{ true, commands.at(random_index).order});
+		}
+		else {
+			order = {
+				current_id++,
+				0,
+				qty,
+				0,
+				OrderType::Market,
+				side,
+			};
+			active_limits_ids.push_back(order.order_id);
+			commands.push_back(Command{ false, order });
+		}
 	}
 
 	for (auto _ : state) {
