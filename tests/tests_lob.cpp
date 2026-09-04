afb6d167a2caf541e80d2dc5e198b1c5e516e7a5
Ratanotam Kehal <47280941+RatanotamKehal@users.noreply.github.com>
2026-09-03 20:05:52 -0700

    Added a createOrder

--- tests/tests_lob.cpp
+++ tests/tests_lob.cpp
@@ -3,4 +3,18 @@
 
 
 namespace lob {
+    Order createOrder(Price price, Quantity quantity, Side side,
+                      OrderType type = OrderType::Limit) {
+        static OrderId id = 1;
+
+        return Order{
+            id++,
+            price,
+            quantity,
+            0,
+            side,
+            type,
+        };
+    }
+
 }
