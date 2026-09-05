07b355780b5f26d1eecc7c5eac793e14bab8e169
Ratanotam Kehal <47280941+RatanotamKehal@users.noreply.github.com>
2026-09-05 16:30:15 -0700

    Updated CMake and created necessary files

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -32,6 +32,16 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/3verything/Coding/GitHub/LimitOrderBook/out/build/x64-Debug/_deps/googletest-build/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/3verything/Coding/GitHub/LimitOrderBook/out/build/x64-Debug/_deps/benchmark-build/cmake_install.cmake")
+endif()
+
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
