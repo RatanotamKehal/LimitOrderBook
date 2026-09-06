2014921c5003984ca1806cb33f489ea64a9da069
Ratanotam Kehal <47280941+RatanotamKehal@users.noreply.github.com>
2026-09-05 17:22:06 -0700

    Updated constructor in lob.cpp

--- src/lob.cpp
+++ src/lob.cpp
@@ -1,27 +1,26 @@
 #include "lob.h"
 #include <cmath>
+#include <limits>
 
-const int MEM_POOL_SIZE{ 1000000 };
-const int ORDER_POOL_SIZE{ std::log2(MEM_POOL_SIZE) };
+constexpr OrderIndex INVALID_INDEX = std::numeric_limits<OrderIndex>::max();
+constexpr int MEM_POOL_SIZE{ 1000000 };
+const int ORDER_POOL_SIZE{ std::pow(2, std::log2(MEM_POOL_SIZE)) };
 
 LOB::LOB() {
-	buy_orders.resize(ORDER_POOL_SIZE);
-	sell_orders.resize(ORDER_POOL_SIZE);
+	buy_orders.resize(ORDER_POOL_SIZE, { INVALID_INDEX, INVALID_INDEX });
+	sell_orders.resize(ORDER_POOL_SIZE, { INVALID_INDEX, INVALID_INDEX });
 	mem_pool.resize(MEM_POOL_SIZE);
+	order_map.resize(MEM_POOL_SIZE, INVALID_INDEX);
 
 	free_list_head = 0;
 
 	for (int i = 0; i < MEM_POOL_SIZE; i++) {
 		mem_pool[i].next = i + 1;
-		mem_pool[i].prev = i - 1;
-
-		if (i == 0) {
-			mem_pool[i].prev = NULL;
-		}
-		if (i == (MEM_POOL_SIZE - 1)) {
-			mem_pool[i].next = NULL;
-		}
 	}
+	mem_pool[MEM_POOL_SIZE - 1].next = INVALID_INDEX;
+
+	best_bid = 0;
+	best_ask = std::numeric_limits<Price>::max();
 }
 
 void LOB::add(Order& order) {
