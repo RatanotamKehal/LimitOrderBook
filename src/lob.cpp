e29ef22c8c6905d7473957e6c0b5e27e5595a102
Ratanotam Kehal <47280941+RatanotamKehal@users.noreply.github.com>
2026-09-05 17:45:56 -0700

    market order for add function

--- src/lob.cpp
+++ src/lob.cpp
@@ -3,8 +3,8 @@
 #include <limits>
 
 constexpr OrderIndex INVALID_INDEX = std::numeric_limits<OrderIndex>::max();
-constexpr int MEM_POOL_SIZE{ 1000000 };
-const int ORDER_POOL_SIZE{ std::pow(2, std::log2(MEM_POOL_SIZE)) };
+constexpr uint64_t MEM_POOL_SIZE{ 1000000 };
+const uint64_t ORDER_POOL_SIZE{ static_cast<uint64_t>(std::pow(2, std::log2(MEM_POOL_SIZE))) };
 
 LOB::LOB() {
 	buy_orders.resize(ORDER_POOL_SIZE, { INVALID_INDEX, INVALID_INDEX });
@@ -23,8 +23,30 @@ LOB::LOB() {
 	best_ask = std::numeric_limits<Price>::max();
 }
 
+
 void LOB::add(Order& order) {
+	if (order.type == OrderType::Market) {
+		if (order.side == Side::Buy) {
+			OrderIndex& index = sell_orders[best_ask * PRICE_MULTIPLIER].head;
+			Order& match = mem_pool[index];
+
+			Quantity min_quantity = std::min(match.quantity, order.quantity);
+			match.quantity -= min_quantity;
+			order.quantity -= order.quantity;
+			
+			if (match.quantity == 0) {
+				index = match.next;
+			}
+		}
+		
+
+
+		return;
+	}
 	
+	if (order.side == Side::Buy) {
+
+	}
 }
 
 void LOB::cancel(uint64_t order_id) {
